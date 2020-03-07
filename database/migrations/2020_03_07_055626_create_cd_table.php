2fa39db01eacbb65948a6d11df8ecf2b42d1baa7
Raindeca Dzulikrom Haqqu <preserved10core@gmail.com>
2020-03-07 14:25:03 +0700

    migrated cd table

---
+++ database/migrations/2020_03_07_055626_create_cd_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCdTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cd', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->float('rate');
+            $table->string('category');
+            $table->integer('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cd');
+    }
+}
